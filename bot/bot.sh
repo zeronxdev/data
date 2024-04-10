174408a26dc2cba78fd99f631df3b761d2c8a2e6
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-10 20:42:20 +0700

    Create bot.sh

---
+++ bot/bot.sh
@@ -0,0 +1,9 @@
+yum update -y && apt update -y
+yum install python3 -t && apt install python3 -y
+pip3 install telegram
+pip3 install psutil
+pip3 install python-telegram-bot==13.7
+pip3 install bs4
+pip3 install requests
+mkdir bot && cd bot
+wget 
